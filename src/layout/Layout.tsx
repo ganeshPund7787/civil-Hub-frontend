a19c70bb958bc8356ac9148114492f568ebd0dce
Ganesh Pund <ganupund7787@gmail.com>
2024-07-12 19:32:53 -0700

    solve ts bug

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -9,7 +9,7 @@ const Layout = ({ children, showHero = false }: Props) => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      {/* {showHero && <Hero />} */}
+      {showHero ? "" : ""}
       <div className="container mx-auto flex-1 py-10">{children}</div>
       {/* <Footer /> */}
     </div>
