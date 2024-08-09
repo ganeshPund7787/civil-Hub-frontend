34cfccfa565d69d57c9a426e6a36a43584b1b669
Ganesh Pund <ganupund7787@gmail.com>
2024-08-09 11:43:05 -0700

    solve ts bug

--- src/pages/Messages.tsx
+++ src/pages/Messages.tsx
@@ -2,14 +2,11 @@
 
 import MsgSidebar from "@/components/Messages/sidebar/MsgSidebar";
 
-// import DemoForm from "./DemoForm";
-
 const Messages = () => {
   return (
     <div className="flex">
       <MsgSidebar />
       {/* <MsgContainer /> */}
-      {/* <DemoForm /> */}
     </div>
   );
 };
